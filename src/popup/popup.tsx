98c53ad6d5c5ff7f18ebeac8f922b32fdd4e459f
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-05-28 18:40:54 +0100

    template ready

---
+++ src/popup/popup.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import '../assets/tailwind.css';
+
+const Test = () => {
+  return (
+    <div>
+      <h1 className="text-5xl  bg-green-500">Hello world</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime aliquid
+        accusamus sint omnis reprehenderit quia quos minima voluptates suscipit
+        nulla eveniet sapiente placeat sed, maiores blanditiis dignissimos
+        assumenda nobis enim?
+      </p>
+    </div>
+  );
+};
+
+const container = document.createElement('div');
+document.body.appendChild(container);
+const root = createRoot(container);
+root.render(<Test />);
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  entry: {
+    popup: path.resolve('./src/popup/popup.tsx'),
+  },
+  module: {
+    rules: [
+      {
+        use: 'ts-loader',
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+      },
+      {
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                ident: 'postcss',
+                plugins: [tailwindcss, autoprefixer],
+              },
+            },
+          },
+        ],
+        test: /\.css$/i,
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve('src/static'),
+          to: path.resolve('dist'),
+        },
+      ],
+    }),
+    new HtmlPlugin({
+      title: 'ReactJs Boilerplate',
+      filename: 'popup.html',
+      chucks: ['popup'],
+    }),
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[name].js',
+  },
+};
